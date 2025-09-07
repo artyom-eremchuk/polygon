906a833f0a559b6f0c0b239a5002c41a84895d81
Artyom Eremchuk <aeremchuk.dev@yandex.ru>
2025-09-07 20:36:36 +0300

    add fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
+  await app.listen(3000);
 }
+
 bootstrap();
